1783416b6bfdd9772a2478fd3bd9fdc55085ed44
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-08-16 21:32:16 +0900

    Add Signup・Login・Logout flow

--- app/src/main/java/com/devtides/coroutinesroom/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devtides/coroutinesroom/viewmodel/LoginViewModel.kt
@@ -1,15 +1,38 @@
 package com.devtides.coroutinesroom.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import com.devtides.coroutinesroom.model.LoginState
+import com.devtides.coroutinesroom.model.UserDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private val db by lazy { UserDatabase(getApplication()).userDao() }
 
     val loginComplete = MutableLiveData<Boolean>()
     val error = MutableLiveData<String>()
 
     fun login(username: String, password: String) {
+        coroutineScope.launch {
+            var user = db.getUser(username)
 
+            withContext(Dispatchers.Main) {
+                if (user == null) {
+                    error.value = "User not found"
+                } else if (user.passwordHash != password.hashCode()) {
+                    error.value = "Password is incorrect"
+                } else {
+                    LoginState.login(user)
+                    loginComplete.value = true
+                }
+            }
+        }
     }
 }
